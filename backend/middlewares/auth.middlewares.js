e0a8bd574944d9353ece6a523bc0a7fee4128fe9
jinilptl <jrpatel1747@gmail.com>
2025-09-29 10:49:44 +0530

    fix: Improve error messages in token verification middleware for clarity

--- backend/middlewares/auth.middlewares.js
+++ backend/middlewares/auth.middlewares.js
@@ -8,12 +8,12 @@ const VerifyToken = (req, res, next) => {
   const token = req.cookies?.token || req.headers?.authorization?.split(" ")[1];
   console.log(token);
   if (!token) {
-    return next(new ApiError(401, "Unauthorized"));
+    return next(new ApiError(401, "Unauthorized at finding token"));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return next(new ApiError(401, "Unauthorized"));
+      return next(new ApiError(401, "Unauthorized at verifying token"));
     }
 
     console.log("decode token is ", decoded);
@@ -30,15 +30,15 @@ const VerifyTokenTest = (req, res, next) => {
   const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
   console.log("test token middleware token is", token);
   if (!token) {
-    return next(new ApiError(401, "Unauthorized"));
+    return next(new ApiError(401, "Unauthorized at finding token in test middleware"));
   }
 
-  console.log("after cheking token");
-  
+  console.log("after checking token");
+
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return next(new ApiError(401, "Unauthorized"));
+      return next(new ApiError(401, "Unauthorized at verifying token in test middleware"));
     }
 
     console.log("decode token is ", decoded);
